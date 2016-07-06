753d106826e53500b1a061641122912f62af5771
HanJian <hanjian1087@gmail.com>
2016-07-06 20:32:34 +0800

    Duplicates push

--- lettCodeSolution/ConsoleApplication1/Solution.cpp
+++ lettCodeSolution/ConsoleApplication1/Solution.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
 #include<vector>
+using namespace std;
+class Solution{
+public:
+	int removeDuplicates(vector<int>& nums){
+		if (nums.empty()) return 0;
+		int index = 0;
+		for (int i = i; i < nums.size(); i++){
+			if (nums[index] != nums[i])
+				nums[++index] = nums[i];
+		}
+	}
+};
