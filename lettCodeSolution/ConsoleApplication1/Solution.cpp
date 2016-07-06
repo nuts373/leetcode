5f59838896bfdea7320a76946880173cda730960
HanJian <hanjian1087@gmail.com>
2016-07-06 21:14:03 +0800

    search push

--- lettCodeSolution/ConsoleApplication1/Solution.cpp
+++ lettCodeSolution/ConsoleApplication1/Solution.cpp
@@ -11,4 +11,40 @@ public:
 				nums[++index] = nums[i];
 		}
 	}
+	int removeDuplicates2(vector<int>& nums){
+		if (nums.size() <= 2) return nums.size();
+		int index = 2;
+		for (int i = 2; i < nums.size(); i++){
+			if (nums[i] != nums[index - 2])
+				nums[index++] = nums[i];
+		}
+		return index;
+	}
+	int search(const vector<int>& nums, int target){
+		int first = 0, last = nums.size();
+		while (first != last){
+			const int mid = first + (last - first) / 2;
+			if (nums[mid] == target)
+				return mid;
+			if (nums[first] <= nums[mid]){
+				if (nums[first] <= target && target < nums[mid])
+					last = mid;
+				else
+					first = mid + 1;
+			}
+			else{
+				if (nums[mid] < target && target <= nums[last - 1])
+					first = mid + 1;
+				else
+					last = mid;
+			}
+		}
+		return -1;
+	}
 };
+
+int main(){
+	Solution solution;
+	vector<int> input{ 4, 5, 6, 7, 0, 1, 2 };
+	cout << solution.search(input, 7) << endl;
+}
