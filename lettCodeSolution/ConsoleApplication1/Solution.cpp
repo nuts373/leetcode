a0068637526ae9888c75f66f2ab87c32c5d30899
hanjian <e4e555@163.com>
2016-07-07 11:08:19 +0800

    singleNumberIII push

--- lettCodeSolution/ConsoleApplication1/Solution.cpp
+++ lettCodeSolution/ConsoleApplication1/Solution.cpp
@@ -67,6 +67,22 @@ public:
 		}
 		return a;
 	}
+	vector<int> singleNumber(vector<int>& nums){
+		int AxorB = 0;
+		for (int i = 0; i < nums.size(); i++){
+			AxorB ^= nums[i];
+		}
+		int mask = AxorB&(~(AxorB - 1));
+		int A = 0, B = 0;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (mask&nums[i])
+				A ^= nums[i];
+			else
+				B ^= nums[i];
+		}
+		return vector<int>({ A, B });
+	}
 };
 
 int main(){
